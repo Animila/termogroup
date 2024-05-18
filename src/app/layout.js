0c5509b02c5134b3131aadd22b71c07b194af0cd
Animila <khristoforov-i@mail.ru>
2024-05-18 20:05:00 +0900

    fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Menu from "../components/menu";
-import {Footer} from "../components/Footer";
+import Menu from "@/components/menu";
+import {Footer} from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 

e29686bb8bc5333fd2e46e1124178f102d1024ca
Animila <khristoforov-i@mail.ru>
2024-05-18 19:36:52 +0900

    dockerfile

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    output: 'export',
-    basePath: process.env.NODE_ENV === 'production' ? '/termogroup' : '',
     trailingSlash: true,
 };
 

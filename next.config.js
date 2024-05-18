4f6ecdd2b17e116681dd417b1d47c1d36552e876
Animila <khristoforov-i@mail.ru>
2024-05-18 20:08:07 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'standalone',
     reactStrictMode: true,
     trailingSlash: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig

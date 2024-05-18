5ab1b5bf6991bfbd029509bd676ea4ec6c2de8fc
Animila <khristoforov-i@mail.ru>
2024-05-18 19:16:36 +0900

    new

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,14 @@
 'use client'
-import HeaderSection from "@/components/header";
-import {CalculatedSection} from "@/components/CalculatePages/CalculatedSection";
-import {Faq} from "@/components/Faq";
-import {Constructor} from "@/components/constructor";
-import {Meeting} from "@/components/Meeting";
-import {InformationVideo} from "@/components/Modals/InformationVideo";
+import HeaderSection from "../components/header";
+import {CalculatedSection} from "../components/CalculatePages/CalculatedSection";
+import {Faq} from "../components/Faq";
+import {Constructor} from "../components/constructor";
+import {Meeting} from "../components/Meeting";
+import {InformationVideo} from "../components/Modals/InformationVideo";
 import {useState} from "react";
-import {Projects} from "@/components/Projects";
-import SpecialOffers from "@/components/Offers";
-import Contacts from "@/components/Contacts";
+import {Projects} from "../components/Projects";
+import SpecialOffers from "../components/Offers";
+import Contacts from "../components/Contacts";
 
 export default function Home() {
     const [activeVideo, setActiveVideo] = useState(false)

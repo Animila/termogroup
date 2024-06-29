d969cded0ec393ae1b3021ba8fe1044d91b35954
Animila <khristoforov-i@mail.ru>
2024-06-30 02:23:51 +0900

    fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,19 +8,20 @@ export const HeaderSection = () => {
   const { changeActiveVideo } = useModal()
 
   const mobileImageVariants1 = {
-    hidden: { opacity: 0, x: -50, y: -50 },
+    hidden: { opacity: 0, x: -100, y: -100 },
     visible: { opacity: 1, x: 0, y: 0, transition: { duration: 1, delay: 0.2 } },
   };
 
   const mobileImageVariants2 = {
-    hidden: { opacity: 0, x: 50, y: -50 },
-    visible: { opacity: 1, x: 0, y: 0, transition: { duration: 1, delay: 0.4 } },
+    hidden: { opacity: 0, x: -100, y: -50 },
+    visible: { opacity: 1, x: 0, y: -70, transition: { duration: 1, delay: 0.4 } },
   };
 
   const mobileImageVariants3 = {
-    hidden: { opacity: 0, x: -50, y: 50 },
-    visible: { opacity: 1, x: 0, y: 0, transition: { duration: 1, delay: 0.6 } },
+    hidden: { opacity: 0, x: -100, y: 100 },
+    visible: { opacity: 1, x: 0,  y: -60, transition: { duration: 1, delay: 0.6 } },
   };
+
   const desktopImageVariants1 = {
     hidden: { opacity: 0, x: -100, y: -100 },
     visible: { opacity: 1, x: 0, y: 0, transition: { duration: 1, delay: 0.2 } },
@@ -47,7 +48,7 @@ export const HeaderSection = () => {
           animate="visible"
           className="sm:px-[10px] sm:mt-[20px] lg:mx-[5vw] 2xl:mx-[15vw] pb-[70px] md:mt-[100px] block bg-white relative"
       >
-        <div className="absolute lg:right-[20px] md:right-0 md:top-[70px] md:translate-x-0 md:overflow-x-visible md:h-[450px] sm:right-[50%] sm:top-[450px] sm:translate-x-[50%] z-0">
+        <div className="absolute lg:right-[20px] md:right-0 md:top-[70px] md:translate-x-0 md:overflow-x-visible md:h-[450px] sm:right-[50%] sm:top-[450px] sm:translate-x-[20%] z-0">
           <motion.div
               initial="hidden"
               animate="visible"
@@ -55,7 +56,7 @@ export const HeaderSection = () => {
               viewport={{ once: true }}
           >
             <Image
-                className="sm:absolute sm:top-[40px] sm:right-[-50px] sm:z-30 md:static md:z-0"
+                className="static z-0"
                 src={"/images/panels/1_1.png"}
                 alt={"Панель 1"}
                 width={500}
@@ -69,7 +70,7 @@ export const HeaderSection = () => {
               viewport={{ once: true }}
           >
             <Image
-                className="absolute md:top-[70px] md:left-[20px] md:right-auto z-20 sm:top-[20px] sm:left-auto sm:right-[60px]"
+                className="absolute md:top-[70px] md:left-[20px] z-20 sm:top-[20px] sm:left-[40px] "
                 src={"/images/panels/1_2.png"}
                 alt={"Панель 2"}
                 width={500}
@@ -83,7 +84,7 @@ export const HeaderSection = () => {
               viewport={{ once: true }}
           >
             <Image
-                className="sm:static sm:right-0 sm:z-10 md:absolute md:top-[170px] md:left-[40px] md:z-30"
+                className="absolute sm:top-[50px] md:top-[170px] sm:left-[80px] md:left-[40px] z-30"
                 src={"/images/panels/1_3.png"}
                 alt={"Панель 3"}
                 width={500}
@@ -109,37 +110,18 @@ export const HeaderSection = () => {
             viewport={{ once: true }}
             className="md:pt-[64px] sm:pt-[150px] flex sm:flex-col sm:gap-[10px] md:flex-row md:gap-[30px] z-10"
         >
-          <div className="h-[100px] sm:border-b-2 sm:border-b-accent_one md:border-0 sm:gap-[20px] md:w-[188px] flex md:flex-col sm:flex-row sm:justify-center md:justify-start md:items-start sm:items-center">
-            <div className="text-[29px] leading-[38px]">32</div>
-            <div className="text-[15px] leading-[19px] mt-[4px] sm:w-[200px]">фактура декоративных панелей на выбор</div>
+          <div className="h-[100px] sm:gap-[20px] md:gap-0 sm:border-b-2 sm:border-b-accent_one md:border-0 md:w-[188px] flex md:flex-col sm:flex-row sm:justify-start md:justify-start md:items-start sm:items-center">
+            <div className="text-[29px] leading-[38px] font-bold w-[50px]">32</div>
+            <div className="text-[15px] leading-[19px] mt-[4px] sm:w-[200px] font-semibold md:text-[#a1a1a1]">фактура декоративных панелей на выбор</div>
           </div>
-          <div className="h-[100px] sm:border-b-2 sm:border-b-accent_one md:border-0 sm:gap-[20px] md:w-[188px] flex md:flex-col sm:flex-row sm:justify-center md:justify-start md:items-start sm:items-center">
-            <div className="text-[29px] leading-[38px]">365</div>
-            <div className="text-[15px] leading-[19px] mt-[4px] sm:w-[200px]">дней в году фасадные панели пригодны для монтажа</div>
+          <div className="h-[100px] sm:gap-[20px] md:gap-0 sm:border-b-2 sm:border-b-accent_one md:border-0 md:w-[188px] flex md:flex-col sm:flex-row sm:justify-start md:justify-start md:items-start sm:items-center">
+            <div className="text-[29px] leading-[38px] font-bold w-[50px]">365</div>
+            <div className="text-[15px] leading-[19px] mt-[4px] sm:w-[200px] font-semibold md:text-[#a1a1a1]">дней в году фасадные панели пригодны для монтажа</div>
           </div>
-          <div className="h-[100px] sm:gap-[20px] md:w-[188px] flex md:flex-col sm:flex-row sm:justify-center md:justify-start md:items-start sm:items-center">
-            <div className="text-[29px] leading-[38px]">{">"}4</div>
-            <div className="text-[15px] leading-[19px] mt-[4px] sm:w-[200px]">килограмм вес одной монтажной плиты</div>
+          <div className="h-[100px] sm:gap-[20px] md:gap-0 md:w-[188px] flex md:flex-col sm:flex-row sm:justify-start md:items-start sm:items-center">
+            <div className="text-[29px] leading-[38px] font-bold w-[50px]">{">"}4</div>
+            <div className="text-[15px] leading-[19px] mt-[4px] sm:w-[200px] font-semibold md:text-[#a1a1a1]">килограмм вес одной монтажной плиты</div>
           </div>
-        </motion.div>
-        <motion.div
-            initial="hidden"
-            animate="visible"
-            variants={textVariants}
-            viewport={{ once: true }}
-            className=" mt-[25px] z-10"
-        >
-          {/*<button className="w-[318px] h-[70px] flex justify-center sm:mx-auto items-center gap-[10px] bg-main_one rounded-[70px]">*/}
-          {/*<span*/}
-          {/*    className="text-[18px] leading-[23px] tracking-[0.3px] text-white"*/}
-          {/*    onClick={() => {*/}
-          {/*      changeActiveVideo();*/}
-          {/*    }}*/}
-          {/*>*/}
-          {/*  Смотрите видео*/}
-          {/*</span>*/}
-          {/*  <PlaySVG />*/}
-          {/*</button>*/}
         </motion.div>
       </motion.section>
   );
